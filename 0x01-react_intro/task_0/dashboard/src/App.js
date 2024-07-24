0e5d49f31ceccc9a986cc5a219dd0c01b389c375
abkalkaled <aminukhaleedabiola@gmail.com>
2024-07-24 21:22:41 +0000

    task 0

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,18 @@
+import logo from './h-logo.jpg;
+import "./App.css"
+
+function App() {
+	return (
+		<div className="App-header">
+		<img src="h_logo.jpg" alt="holberton logo">
+		<h1>School dashboard</h1>
+		</div>
+		<div className="App-body">
+		<p>Login to access the full dashboard</p>
+		</div>
+		<div className="App-footer">
+		<p> Copyright 2020 - holberton School </p>
+		</div>
+	);
+};
+export default App;
